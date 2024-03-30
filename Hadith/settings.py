4bd08f1936c733e243e9a4f2a152883cbd41d6eb
unknown <momenmedhatsalem@gmail.com>
2024-03-30 06:47:46 +0200

    moved gitignore

--- Hadith/settings.py
+++ Hadith/settings.py
@@ -18,9 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-62p9ax(d8c@j6+a14egh23jaq87lt4y&h6s**qiz_l4rw7(8-='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
